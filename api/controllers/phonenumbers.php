c5ca0abf2f08b05cd8ae9a17b4f21d57c0801676
John Wheatley <john.wheatley@sendsteps.com>
2018-07-25 11:30:54 +0200

    Seperate Record function

--- api/controllers/phonenumbers.php
+++ api/controllers/phonenumbers.php
@@ -2,7 +2,14 @@
     require(__DIR__.'/../base/nova-api.php');
     class Phonenumbers extends NovaAPI {
         function getOverview(){
-            $results = $this->query('SELECT * FROM phonenumbers LEFT JOIN countries ON phonenumbers.countryIsoCode=countries.isoCode WHERE phonenumbers.isDeleted != 1;;');
+            $results = $this->query('SELECT * FROM phonenumbers LEFT JOIN countries ON phonenumbers.countryIsoCode=countries.isoCode WHERE phonenumbers.isDeleted != 1;');
+            $return = array(
+                'content' => $results
+            );
+            echo json_encode($return);
+        }
+        function getRecord(){
+            $results = $this->query('SELECT * FROM phonenumbers LEFT JOIN countries ON phonenumbers.countryIsoCode=countries.isoCode WHERE phonenumbers.isDeleted != 1 AND id = 1;');
             $return = array(
                 'content' => $results
             );
