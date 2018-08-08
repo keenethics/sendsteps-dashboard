c8d05639c257317a8a54a943ff12bb3d2c540731
John Wheatley <john.wheatley@sendsteps.com>
2018-08-08 16:16:50 +0200

    Typo

--- api/controllers/phonenumbers.php
+++ api/controllers/phonenumbers.php
@@ -3,7 +3,7 @@ require_once __DIR__.'/../base/nova-api.php';
 
 class Phonenumbers extends NovaAPI {
     function getOverview(){
-        $results = $this->query('SELECT * FROM phonenumbers LEFT JOIN countries ON phonenumbers.countryIsoCode=countries.isoCode WHERE phonenumbers.isDeleted != 1;;');
+        $results = $this->query('SELECT * FROM phonenumbers LEFT JOIN countries ON phonenumbers.countryIsoCode=countries.isoCode WHERE phonenumbers.isDeleted != 1;');
         return json_encode(['content' => $results]);
     }
 
