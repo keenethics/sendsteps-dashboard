44f069c6529651f110c7f8f0c268adf0a46c2a87
John Wheatley <john.wheatley@sendsteps.com>
2018-08-08 11:01:34 +0200

    Converted API to a try catch pattern

--- api/index.php
+++ api/index.php
@@ -1,44 +1,43 @@
-<?php 
-
-    //We need some authentication here & a fallback to the login page.
+<?php
+    require_once __DIR__."/base/errors.php";//Load Errors (just in case);
     
-    if ($_POST) {
-        
-        require_once __DIR__."/base/errors.php";//Load Errors (just in case);
+    try { 
+        throw new Exception();
         //1 request = 1 function from 1 controller
+        if(!isset($_POST['controller']) OR !isset($_POST['function'])){
+            throw new Exception ('SpecifyControllerAndFunction');
+        }
+        //Setup Variables & load controller
         $controller_name = $_POST['controller'];
         $function = $_POST['function'];
-        
         $params = (isset($_POST['params']))? implode(',', explode('---', $_POST['params'])) : array();
-        
-        //Some issue with safe mode on this.
-        // if (file_exists("/controllers/$controller_name.php") == false){
-        //     echo json_encode(['error' => stream_resolve_include_path("/controllers/$controller_name.php")]);
-        // }
-        
         require_once __DIR__."/controllers/$controller_name.php";
         $controller_name = ucfirst($controller_name); // Controller Classes have the first letter uppercase
         
-        //Return error, if controller class does not exist
+        //Check controller class exists
         if (!class_exists($controller_name)) {
-            $api = new NovaAPI;
-            $api->setHeaders();
-            echo json_encode(['error' => $novaErrors['ControllerDoesNotExist']]);
-            exit();
+            throw new Exception ('ControllerDoesNotExist');
         }
         
         $controller = new $controller_name;
         $controller->setHeaders();
         
-        //Return error, if method does not exist
+        //Check method/function exists
         if (!method_exists($controller, $function)) {
-            $api = new NovaAPI;
-            $api->setHeaders();
-            echo json_encode(['error' => $novaErrors['MethodDoesNotExist']]);
-            exit();
+            throw new Exception ('MethodDoesNotExist');
         }
         
         $result = $controller->$function($params);
         echo $result;
         exit();
+        
+    } catch (Exception $e) {
+        if ($e->getMessage() == ''){
+            echo 'Error: Undefined error with Nova-API';
+        } else {
+            echo 'Error: '. $novaErrors[$e->getMessage()];exit();   
+        }
     }
+
+
+ 
