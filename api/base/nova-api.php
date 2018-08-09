c382e786e1e8cf12949d034a51bf253071e8b182
John Wheatley <john.wheatley@sendsteps.com>
2018-08-09 10:28:48 +0200

    Call API from anywhere

--- api/base/nova-api.php
+++ api/base/nova-api.php
@@ -9,7 +9,7 @@
         public function setHeaders(){
             header_remove();// clear the old headers
             header('Content-Type: application/json');
-            header('Access-Control-Allow-Origin: http://localhost:3000');
+            header('Access-Control-Allow-Origin: *');
             header('Content-type:application/json;charset=utf-8');
             header("Cache-Control: no-transform,public,max-age=300,s-maxage=900");
             return;
