8235f05f1741e2b997a4fba3c2710ca71adcdda1
John Wheatley <john.wheatley@sendsteps.com>
2018-08-31 11:12:12 +0200

    Params Refined

--- api-nova/index.php
+++ api-nova/index.php
@@ -8,7 +8,7 @@
         //Setup Variables & load controller
         $controller_name = $_POST['controller'];
         $function = $_POST['function'];
-        $params = (isset($_POST['params']))? implode(',', explode('---', $_POST['params'])) : array();
+        $params = (isset($_POST['params']))? explode('---', $_POST['params']) : array();
         
         if (!in_array($controller_name.'.php', scandir(__DIR__."/controllers/"))) {
             throw new Exception('ControllerFileDoesNotExist');    
