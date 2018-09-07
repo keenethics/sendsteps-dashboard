f1cd7d1ed9dc1848eb78d0c89c9b88b283be26a8
John Wheatley <john.wheatley@sendsteps.com>
2018-09-07 13:26:20 +0200

    Step 2: Unifed model for APIs

--- api-common/base/model.php
+++ api-common/base/model.php
@@ -1,8 +1,8 @@
 <?php 
 
-    require_once __DIR__.'/../../api-common/base/base.php';
-    require_once __DIR__.'/../../api-common/errors.php';//Load Errors (just in case);
-    require_once __DIR__.'/../../api-common/medoo/src/Medoo.php';
+    require_once __DIR__.'/../base/base.php';
+    require_once __DIR__.'/../errors.php';//Load Errors (just in case);
+    require_once __DIR__.'/../medoo/src/Medoo.php';
     use Medoo\Medoo;
 
     class Model extends Base{
