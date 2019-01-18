44520273b9530ed01feec434fa51c684d794c559
John Wheatley <john.wheatley@sendsteps.com>
2019-01-18 13:15:25 +0100

    More Testing

--- api-common/dboptions.php
+++ api-common/dboptions.php
@@ -1,10 +1,12 @@
 <?php 
-$db_options = array(
-    // Required
+$db_baseoptions = array(
     'database_type' => 'mysql',
     'database_name' => 'addins',
     'server' => 'localhost',
     'username' => 'root',
     'password' => '',
-    'port' => 3306,
+    'port' => 3306
 );
+
+
+// var_dump($db_baseoptions);exit();
