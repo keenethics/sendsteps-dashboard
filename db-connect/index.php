7760872f98d41151a56e900374f52d0dcd506c5c
John Wheatley <john.wheatley@sendsteps.com>
2019-02-08 14:30:02 +0100

    Var_dump test

--- db-connect/index.php
+++ db-connect/index.php
@@ -2,6 +2,8 @@
     <?php 
     
         require __DIR__."/inc/mysql.inc.php";
+        
+        var_dump('Test');exit();
         $db = MySQL::GetConnection();
 
         $db->Prepare("SELECT `id`, `currentSessionId` FROM `Votes` WHERE `id` = ?");
