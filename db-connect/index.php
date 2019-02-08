6856cd773efad94296a0c354e52b7d4362718263
John Wheatley <john.wheatley@sendsteps.com>
2019-02-08 15:11:15 +0100

    Again

--- db-connect/index.php
+++ db-connect/index.php
@@ -1,12 +1,16 @@
 
     <?php 
-        $filepath = __DIR__."/inc/mysql.inc.php";
-        if (!file_exists($filepath)){
-            var_dump("File $filepath does not exist");
-        } else {
-            include $filepath;    
+        $filepaths[] = __DIR__."/inc/settings.php";
+        $filepaths[] = __DIR__."/inc/mysql.inc.php";
+        foreach ($filepaths as $filepath){
+            
+            if (!file_exists($filepath)){
+                var_dump("File $filepath does not exist");
+            } else {
+                include $filepath;    
+            }
+            
         }
-        
     // var_dump('Test');exit();
         $class = 'MySQL';
         if (!class_exists($class, false)) {
