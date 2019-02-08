f1134e47bd437729b8b907efbdbd8e5c19089df7
John Wheatley <john.wheatley@sendsteps.com>
2019-02-08 15:24:56 +0100

    Check that result caching is not enabled

--- db-connect/index.php
+++ db-connect/index.php
@@ -1,22 +1,26 @@
 
     <?php 
-        $filepaths[] = __DIR__."/inc/settings.php";
-        $filepaths[] = __DIR__."/inc/mysql.inc.php";
-        foreach ($filepaths as $filepath){
+        // $filepaths[] = __DIR__."/inc/settings.php";
+        // $filepaths[] = __DIR__."/inc/mysql.inc.php";
+        // foreach ($filepaths as $filepath){
             
-            if (!file_exists($filepath)){
-                var_dump("File $filepath does not exist");
-            } else {
-                include $filepath;    
-            }
+        //     if (!file_exists($filepath)){
+        //         var_dump("File $filepath does not exist");
+        //     } else {
+        //         require $filepath;    
+        //     }
             
-        }
+        // }
         // $class = 'MySQL';
         // if (!class_exists($class, false)) {
         //     var_dump("Unable to load class: $class");
         // } else {
+            
+            require __DIR__."/inc/settings.php";
+            require __DIR__."/inc/mysql.inc.php";
+            
             $db = MySQL::GetConnection();
-            $db->Prepare("SELECT * FROM `users` WHERE `email` LIKE ?");
+            $db->Prepare("SELECT id FROM `users` WHERE `email` LIKE ?");
     
             $email = 'john.wheatley@sendsteps.com';
             $db->BindValue(1, $email);
