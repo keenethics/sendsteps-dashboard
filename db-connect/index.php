fd69297c9eeb289b2a27899a430e59fa2b8c62ac
John Wheatley <john.wheatley@sendsteps.com>
2019-02-08 14:36:57 +0100

    Check class exists

--- db-connect/index.php
+++ db-connect/index.php
@@ -1,10 +1,13 @@
 
     <?php 
     
-echo file_exists(__DIR__."/inc/mysql.inc.php");
-    var_dump('Test');exit();
-        require __DIR__."/inc/mysql.inc.php";
-        
+// echo file_exists(__DIR__."/inc/mysql.inc.php");
+    // var_dump('Test');exit();
+        include __DIR__."/inc/mysql.inc.php";
+        $class = 'MySQL';
+        if (!class_exists($class, false)) {
+            var_dump("Unable to load class: $class");
+        }
         $db = MySQL::GetConnection();
 
         $db->Prepare("SELECT `id`, `currentSessionId` FROM `Votes` WHERE `id` = ?");
