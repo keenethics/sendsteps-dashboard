a807330cfd190aa4a749223cd03a20c8f3d4f07c
Bryan Overduin <bryan.overduin@sendsteps.com>
2019-03-04 14:40:59 +0100

    Attempt with compiled js

--- dashboard/src/scripts/dynamicImports.jsx
+++ dashboard/src/scripts/dynamicImports.jsx
@@ -1,206 +1,315 @@
-import React from 'react';
-import DynamicImport from "../pages/base/DynamicImport";
-import LoadingView from '../pages/base/LoadingView';
+"use strict";
 
-export const Home = props => (
-	<DynamicImport load={() => import('../pages/home/Home')}>
-		{(Component) => Component === null 
-		? <LoadingView />
-		: <Component {...props} />}
-	</DynamicImport>
-)
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.UserOverview = exports.SessionDetails = exports.SessionOverview = exports.DeleteUsersOverview = exports.PhonenumberDetails = exports.PhonenumbersOverview = exports.EditDashboardDetails = exports.EditDashboardOverview = exports.TranslationsDetails = exports.TranslationsOverview = exports.SurveyResultsOverview = exports.PresentationsDetails = exports.PresentationsOverview = exports.MessageFilterLayout = exports.ResponsesiteDetails = exports.SurveyDetails = exports.SurveyOverview = exports.AudienceDetails = exports.ResponsesiteSettings = exports.ResponsesiteLayout = exports.AboutSendsteps = exports.AboutHowItWorks = exports.AboutDashboard = exports.PageNotFound = exports.Home = void 0;
 
-export const PageNotFound = props => (
-	<DynamicImport load={() => import("../pages/base/PageNotFound")}>
-		{(Component) => Component === null 
-		? <LoadingView />
-		: <Component {...props} />}
-	</DynamicImport>
-)
+var _react = _interopRequireDefault(require("react"));
 
-export const AboutDashboard = props => (
-	<DynamicImport load={() => import("../pages/about/dashboard")}>
-		{(Component) => Component === null 
-		? <LoadingView />
-		: <Component {...props} />}
-	</DynamicImport>
-)
+var _DynamicImport = _interopRequireDefault(require("../pages/base/DynamicImport"));
 
-export const AboutHowItWorks = props => (
-	<DynamicImport load={() => import("../pages/about/howitworks")}>
-		{(Component) => Component === null 
-		? <LoadingView />
-		: <Component {...props} />}
-	</DynamicImport>
-)
+var _LoadingView = _interopRequireDefault(require("../pages/base/LoadingView"));
 
-export const AboutSendsteps = props => (
-	<DynamicImport load={() => import("../pages/about/sendsteps")}>
-		{(Component) => Component === null 
-		? <LoadingView />
-		: <Component {...props} />}
-	</DynamicImport>
-)
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-export const ResponsesiteLayout = props => (
-	<DynamicImport load={() => import("../pages/session-before/responsesite-layout/Overview")}>
-		{(Component) => Component === null 
-		? <LoadingView />
-		: <Component {...props} />}
-	</DynamicImport>
-)
+var Home = function Home(props) {
+  return _react.default.createElement(_DynamicImport.default, {
+    load: function load() {
+      return import('../pages/home/Home');
+    }
+  }, function (Component) {
+    return Component === null ? _react.default.createElement(_LoadingView.default, null) : _react.default.createElement(Component, props);
+  });
+};
 
-export const ResponsesiteSettings = props => (
-	<DynamicImport load={() => import("../pages/session-before/responsesite-settings/Overview")}>
-		{(Component) => Component === null 
-		? <LoadingView />
-		: <Component {...props} />}
-	</DynamicImport>
-)
+exports.Home = Home;
 
-export const AudienceDetails = props => (
-	<DynamicImport load={() => import("../pages/session-before/audience-identification/Overview")}>
-		{(Component) => Component === null 
-		? <LoadingView />
-		: <Component {...props} />}
-	</DynamicImport>
-)
+var PageNotFound = function PageNotFound(props) {
+  return _react.default.createElement(_DynamicImport.default, {
+    load: function load() {
+      return import("../pages/base/PageNotFound");
+    }
+  }, function (Component) {
+    return Component === null ? _react.default.createElement(_LoadingView.default, null) : _react.default.createElement(Component, props);
+  });
+};
 
-export const SurveyOverview = props => (
-	<DynamicImport load={() => import("../pages/session-before/surveys/Overview")}>
-		{(Component) => Component === null 
-		? <LoadingView />
-		: <Component {...props} />}
-	</DynamicImport>
-)
+exports.PageNotFound = PageNotFound;
 
-export const SurveyDetails = props => (
-	<DynamicImport load={() => import("../pages/session-before/surveys/Details")}>
-		{(Component) => Component === null 
-		? <LoadingView />
-		: <Component {...props} />}
-	</DynamicImport>
-)
+var AboutDashboard = function AboutDashboard(props) {
+  return _react.default.createElement(_DynamicImport.default, {
+    load: function load() {
+      return import("../pages/about/dashboard");
+    }
+  }, function (Component) {
+    return Component === null ? _react.default.createElement(_LoadingView.default, null) : _react.default.createElement(Component, props);
+  });
+};
 
-export const ResponsesiteDetails = props => (
-	<DynamicImport load={() => import("../pages/session-before/response-website/Overview")}>
-		{(Component) => Component === null 
-		? <LoadingView />
-		: <Component {...props} />}
-	</DynamicImport>
-)
+exports.AboutDashboard = AboutDashboard;
 
-export const MessageFilterLayout = props => (
-	<DynamicImport load={() => import("../pages/session-during/message-filter/Overview")}>
-		{(Component) => Component === null 
-		? <LoadingView />
-		: <Component {...props} />}
-	</DynamicImport>
-)
+var AboutHowItWorks = function AboutHowItWorks(props) {
+  return _react.default.createElement(_DynamicImport.default, {
+    load: function load() {
+      return import("../pages/about/howitworks");
+    }
+  }, function (Component) {
+    return Component === null ? _react.default.createElement(_LoadingView.default, null) : _react.default.createElement(Component, props);
+  });
+};
 
-export const PresentationsOverview = props => (
-	<DynamicImport load={() => import("../pages/session-results/presentations/Overview")}>
-		{(Component) => Component === null 
-		? <LoadingView />
-		: <Component {...props} />}
-	</DynamicImport>
-)
+exports.AboutHowItWorks = AboutHowItWorks;
 
-export const PresentationsDetails = props => (
-	<DynamicImport load={() => import("../pages/session-results/presentations/Details")}>
-		{(Component) => Component === null 
-		? <LoadingView />
-		: <Component {...props} />}
-	</DynamicImport>
-)
+var AboutSendsteps = function AboutSendsteps(props) {
+  return _react.default.createElement(_DynamicImport.default, {
+    load: function load() {
+      return import("../pages/about/sendsteps");
+    }
+  }, function (Component) {
+    return Component === null ? _react.default.createElement(_LoadingView.default, null) : _react.default.createElement(Component, props);
+  });
+};
 
-export const SurveyResultsOverview = props => (
-	<DynamicImport load={() => import("../pages/session-results/surveys/Overview")}>
-		{(Component) => Component === null 
-		? <LoadingView />
-		: <Component {...props} />}
-	</DynamicImport>
-)
+exports.AboutSendsteps = AboutSendsteps;
 
-// Superadmin
+var ResponsesiteLayout = function ResponsesiteLayout(props) {
+  return _react.default.createElement(_DynamicImport.default, {
+    load: function load() {
+      return import("../pages/session-before/responsesite-layout/Overview");
+    }
+  }, function (Component) {
+    return Component === null ? _react.default.createElement(_LoadingView.default, null) : _react.default.createElement(Component, props);
+  });
+};
 
-export const TranslationsOverview = props => (
-	<DynamicImport load={() => import("../pages/superadmin/translations/Overview")}>
-		{(Component) => Component === null 
-		? <LoadingView />
-		: <Component {...props} />}
-	</DynamicImport>
-)
+exports.ResponsesiteLayout = ResponsesiteLayout;
 
-export const TranslationsDetails = props => (
-	<DynamicImport load={() => import("../pages/superadmin/translations/Details")}>
-		{(Component) => Component === null 
-		? <LoadingView />
-		: <Component {...props} />}
-	</DynamicImport>
-)
+var ResponsesiteSettings = function ResponsesiteSettings(props) {
+  return _react.default.createElement(_DynamicImport.default, {
+    load: function load() {
+      return import("../pages/session-before/responsesite-settings/Overview");
+    }
+  }, function (Component) {
+    return Component === null ? _react.default.createElement(_LoadingView.default, null) : _react.default.createElement(Component, props);
+  });
+};
 
-export const EditDashboardOverview = props => (
-	<DynamicImport load={() => import("../pages/superadmin/edit-dashboard/Overview")}>
-		{(Component) => Component === null 
-		? <LoadingView />
-		: <Component {...props} />}
-	</DynamicImport>
-)
+exports.ResponsesiteSettings = ResponsesiteSettings;
 
-export const EditDashboardDetails = props => (
-	<DynamicImport load={() => import("../pages/superadmin/edit-dashboard/Details")}>
-		{(Component) => Component === null 
-		? <LoadingView />
-		: <Component {...props} />}
-	</DynamicImport>
-)
+var AudienceDetails = function AudienceDetails(props) {
+  return _react.default.createElement(_DynamicImport.default, {
+    load: function load() {
+      return import("../pages/session-before/audience-identification/Overview");
+    }
+  }, function (Component) {
+    return Component === null ? _react.default.createElement(_LoadingView.default, null) : _react.default.createElement(Component, props);
+  });
+};
 
-export const PhonenumbersOverview = props => (
-	<DynamicImport load={() => import("../pages/superadmin/phonenumbers/Overview")}>
-		{(Component) => Component === null 
-		? <LoadingView />
-		: <Component {...props} />}
-	</DynamicImport>
-)
+exports.AudienceDetails = AudienceDetails;
 
-export const PhonenumberDetails = props => (
-	<DynamicImport load={() => import("../pages/superadmin/phonenumbers/Details")}>
-		{(Component) => Component === null 
-		? <LoadingView />
-		: <Component {...props} />}
-	</DynamicImport>
-)
+var SurveyOverview = function SurveyOverview(props) {
+  return _react.default.createElement(_DynamicImport.default, {
+    load: function load() {
+      return import("../pages/session-before/surveys/Overview");
+    }
+  }, function (Component) {
+    return Component === null ? _react.default.createElement(_LoadingView.default, null) : _react.default.createElement(Component, props);
+  });
+};
 
-export const DeleteUsersOverview = props => (
-	<DynamicImport load={() => import("../pages/superadmin/delete-users/Overview")}>
-		{(Component) => Component === null 
-		? <LoadingView />
-		: <Component {...props} />}
-	</DynamicImport>
-)
+exports.SurveyOverview = SurveyOverview;
 
-export const SessionOverview = props => (
-	<DynamicImport load={() => import("../pages/superadmin/sessions/Overview")}>
-		{(Component) => Component === null 
-		? <LoadingView />
-		: <Component {...props} />}
-	</DynamicImport>
-)
+var SurveyDetails = function SurveyDetails(props) {
+  return _react.default.createElement(_DynamicImport.default, {
+    load: function load() {
+      return import("../pages/session-before/surveys/Details");
+    }
+  }, function (Component) {
+    return Component === null ? _react.default.createElement(_LoadingView.default, null) : _react.default.createElement(Component, props);
+  });
+};
 
-export const SessionDetails = props => (
-	<DynamicImport load={() => import("../pages/superadmin/sessions/Details")}>
-		{(Component) => Component === null 
-		? <LoadingView />
-		: <Component {...props} />}
-	</DynamicImport>
-)
+exports.SurveyDetails = SurveyDetails;
 
-export const UserOverview = props => (
-	<DynamicImport load={() => import("../pages/user/Overview")}>
-		{(Component) => Component === null 
-		? <LoadingView />
-		: <Component {...props} />}
-	</DynamicImport>
-)
+var ResponsesiteDetails = function ResponsesiteDetails(props) {
+  return _react.default.createElement(_DynamicImport.default, {
+    load: function load() {
+      return import("../pages/session-before/response-website/Overview");
+    }
+  }, function (Component) {
+    return Component === null ? _react.default.createElement(_LoadingView.default, null) : _react.default.createElement(Component, props);
+  });
+};
 
+exports.ResponsesiteDetails = ResponsesiteDetails;
+
+var MessageFilterLayout = function MessageFilterLayout(props) {
+  return _react.default.createElement(_DynamicImport.default, {
+    load: function load() {
+      return import("../pages/session-during/message-filter/Overview");
+    }
+  }, function (Component) {
+    return Component === null ? _react.default.createElement(_LoadingView.default, null) : _react.default.createElement(Component, props);
+  });
+};
+
+exports.MessageFilterLayout = MessageFilterLayout;
+
+var PresentationsOverview = function PresentationsOverview(props) {
+  return _react.default.createElement(_DynamicImport.default, {
+    load: function load() {
+      return import("../pages/session-results/presentations/Overview");
+    }
+  }, function (Component) {
+    return Component === null ? _react.default.createElement(_LoadingView.default, null) : _react.default.createElement(Component, props);
+  });
+};
+
+exports.PresentationsOverview = PresentationsOverview;
+
+var PresentationsDetails = function PresentationsDetails(props) {
+  return _react.default.createElement(_DynamicImport.default, {
+    load: function load() {
+      return import("../pages/session-results/presentations/Details");
+    }
+  }, function (Component) {
+    return Component === null ? _react.default.createElement(_LoadingView.default, null) : _react.default.createElement(Component, props);
+  });
+};
+
+exports.PresentationsDetails = PresentationsDetails;
+
+var SurveyResultsOverview = function SurveyResultsOverview(props) {
+  return _react.default.createElement(_DynamicImport.default, {
+    load: function load() {
+      return import("../pages/session-results/surveys/Overview");
+    }
+  }, function (Component) {
+    return Component === null ? _react.default.createElement(_LoadingView.default, null) : _react.default.createElement(Component, props);
+  });
+}; // Superadmin
+
+
+exports.SurveyResultsOverview = SurveyResultsOverview;
+
+var TranslationsOverview = function TranslationsOverview(props) {
+  return _react.default.createElement(_DynamicImport.default, {
+    load: function load() {
+      return import("../pages/superadmin/translations/Overview");
+    }
+  }, function (Component) {
+    return Component === null ? _react.default.createElement(_LoadingView.default, null) : _react.default.createElement(Component, props);
+  });
+};
+
+exports.TranslationsOverview = TranslationsOverview;
+
+var TranslationsDetails = function TranslationsDetails(props) {
+  return _react.default.createElement(_DynamicImport.default, {
+    load: function load() {
+      return import("../pages/superadmin/translations/Details");
+    }
+  }, function (Component) {
+    return Component === null ? _react.default.createElement(_LoadingView.default, null) : _react.default.createElement(Component, props);
+  });
+};
+
+exports.TranslationsDetails = TranslationsDetails;
+
+var EditDashboardOverview = function EditDashboardOverview(props) {
+  return _react.default.createElement(_DynamicImport.default, {
+    load: function load() {
+      return import("../pages/superadmin/edit-dashboard/Overview");
+    }
+  }, function (Component) {
+    return Component === null ? _react.default.createElement(_LoadingView.default, null) : _react.default.createElement(Component, props);
+  });
+};
+
+exports.EditDashboardOverview = EditDashboardOverview;
+
+var EditDashboardDetails = function EditDashboardDetails(props) {
+  return _react.default.createElement(_DynamicImport.default, {
+    load: function load() {
+      return import("../pages/superadmin/edit-dashboard/Details");
+    }
+  }, function (Component) {
+    return Component === null ? _react.default.createElement(_LoadingView.default, null) : _react.default.createElement(Component, props);
+  });
+};
+
+exports.EditDashboardDetails = EditDashboardDetails;
+
+var PhonenumbersOverview = function PhonenumbersOverview(props) {
+  return _react.default.createElement(_DynamicImport.default, {
+    load: function load() {
+      return import("../pages/superadmin/phonenumbers/Overview");
+    }
+  }, function (Component) {
+    return Component === null ? _react.default.createElement(_LoadingView.default, null) : _react.default.createElement(Component, props);
+  });
+};
+
+exports.PhonenumbersOverview = PhonenumbersOverview;
+
+var PhonenumberDetails = function PhonenumberDetails(props) {
+  return _react.default.createElement(_DynamicImport.default, {
+    load: function load() {
+      return import("../pages/superadmin/phonenumbers/Details");
+    }
+  }, function (Component) {
+    return Component === null ? _react.default.createElement(_LoadingView.default, null) : _react.default.createElement(Component, props);
+  });
+};
+
+exports.PhonenumberDetails = PhonenumberDetails;
+
+var DeleteUsersOverview = function DeleteUsersOverview(props) {
+  return _react.default.createElement(_DynamicImport.default, {
+    load: function load() {
+      return import("../pages/superadmin/delete-users/Overview");
+    }
+  }, function (Component) {
+    return Component === null ? _react.default.createElement(_LoadingView.default, null) : _react.default.createElement(Component, props);
+  });
+};
+
+exports.DeleteUsersOverview = DeleteUsersOverview;
+
+var SessionOverview = function SessionOverview(props) {
+  return _react.default.createElement(_DynamicImport.default, {
+    load: function load() {
+      return import("../pages/superadmin/sessions/Overview");
+    }
+  }, function (Component) {
+    return Component === null ? _react.default.createElement(_LoadingView.default, null) : _react.default.createElement(Component, props);
+  });
+};
+
+exports.SessionOverview = SessionOverview;
+
+var SessionDetails = function SessionDetails(props) {
+  return _react.default.createElement(_DynamicImport.default, {
+    load: function load() {
+      return import("../pages/superadmin/sessions/Details");
+    }
+  }, function (Component) {
+    return Component === null ? _react.default.createElement(_LoadingView.default, null) : _react.default.createElement(Component, props);
+  });
+};
+
+exports.SessionDetails = SessionDetails;
+
+var UserOverview = function UserOverview(props) {
+  return _react.default.createElement(_DynamicImport.default, {
+    load: function load() {
+      return import("../pages/user/Overview");
+    }
+  }, function (Component) {
+    return Component === null ? _react.default.createElement(_LoadingView.default, null) : _react.default.createElement(Component, props);
+  });
+};
+
+exports.UserOverview = UserOverview;
