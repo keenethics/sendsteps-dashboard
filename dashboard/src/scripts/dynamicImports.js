1e8b5e25b0b1e86bdcb870606ab5537e654205fb
Bryan Overduin <bryan.overduin@sendsteps.com>
2019-03-04 13:58:57 +0100

    dev deploy test

--- dashboard/src/scripts/dynamicImports.js
+++ dashboard/src/scripts/dynamicImports.js
@@ -3,7 +3,7 @@ import DynamicImport from "../pages/base/DynamicImport";
 import LoadingView from '../pages/base/LoadingView';
 
 export const Home = props => (
-	<DynamicImport load={() => import('../pages/home/Home')}>
+	<DynamicImport load={() => require('../pages/home/Home')}>
 		{(Component) => Component === null 
 		? <LoadingView />
 		: <Component {...props} />}
@@ -11,7 +11,7 @@ export const Home = props => (
 )
 
 export const PageNotFound = props => (
-	<DynamicImport load={() => import("../pages/base/PageNotFound")}>
+	<DynamicImport load={() => require("../pages/base/PageNotFound")}>
 		{(Component) => Component === null 
 		? <LoadingView />
 		: <Component {...props} />}
@@ -19,7 +19,7 @@ export const PageNotFound = props => (
 )
 
 export const AboutDashboard = props => (
-	<DynamicImport load={() => import("../pages/about/dashboard")}>
+	<DynamicImport load={() => require("../pages/about/dashboard")}>
 		{(Component) => Component === null 
 		? <LoadingView />
 		: <Component {...props} />}
@@ -27,7 +27,7 @@ export const AboutDashboard = props => (
 )
 
 export const AboutHowItWorks = props => (
-	<DynamicImport load={() => import("../pages/about/howitworks")}>
+	<DynamicImport load={() => require("../pages/about/howitworks")}>
 		{(Component) => Component === null 
 		? <LoadingView />
 		: <Component {...props} />}
@@ -35,7 +35,7 @@ export const AboutHowItWorks = props => (
 )
 
 export const AboutSendsteps = props => (
-	<DynamicImport load={() => import("../pages/about/sendsteps")}>
+	<DynamicImport load={() => require("../pages/about/sendsteps")}>
 		{(Component) => Component === null 
 		? <LoadingView />
 		: <Component {...props} />}
@@ -43,7 +43,7 @@ export const AboutSendsteps = props => (
 )
 
 export const ResponsesiteLayout = props => (
-	<DynamicImport load={() => import("../pages/session-before/responsesite-layout/Overview")}>
+	<DynamicImport load={() => require("../pages/session-before/responsesite-layout/Overview")}>
 		{(Component) => Component === null 
 		? <LoadingView />
 		: <Component {...props} />}
@@ -51,7 +51,7 @@ export const ResponsesiteLayout = props => (
 )
 
 export const ResponsesiteSettings = props => (
-	<DynamicImport load={() => import("../pages/session-before/responsesite-settings/Overview")}>
+	<DynamicImport load={() => require("../pages/session-before/responsesite-settings/Overview")}>
 		{(Component) => Component === null 
 		? <LoadingView />
 		: <Component {...props} />}
@@ -59,7 +59,7 @@ export const ResponsesiteSettings = props => (
 )
 
 export const AudienceDetails = props => (
-	<DynamicImport load={() => import("../pages/session-before/audience-identification/Overview")}>
+	<DynamicImport load={() => require("../pages/session-before/audience-identification/Overview")}>
 		{(Component) => Component === null 
 		? <LoadingView />
 		: <Component {...props} />}
@@ -67,7 +67,7 @@ export const AudienceDetails = props => (
 )
 
 export const SurveyOverview = props => (
-	<DynamicImport load={() => import("../pages/session-before/surveys/Overview")}>
+	<DynamicImport load={() => require("../pages/session-before/surveys/Overview")}>
 		{(Component) => Component === null 
 		? <LoadingView />
 		: <Component {...props} />}
@@ -75,7 +75,7 @@ export const SurveyOverview = props => (
 )
 
 export const SurveyDetails = props => (
-	<DynamicImport load={() => import("../pages/session-before/surveys/Details")}>
+	<DynamicImport load={() => require("../pages/session-before/surveys/Details")}>
 		{(Component) => Component === null 
 		? <LoadingView />
 		: <Component {...props} />}
@@ -83,7 +83,7 @@ export const SurveyDetails = props => (
 )
 
 export const ResponsesiteDetails = props => (
-	<DynamicImport load={() => import("../pages/session-before/response-website/Overview")}>
+	<DynamicImport load={() => require("../pages/session-before/response-website/Overview")}>
 		{(Component) => Component === null 
 		? <LoadingView />
 		: <Component {...props} />}
@@ -91,7 +91,7 @@ export const ResponsesiteDetails = props => (
 )
 
 export const MessageFilterLayout = props => (
-	<DynamicImport load={() => import("../pages/session-during/message-filter/Overview")}>
+	<DynamicImport load={() => require("../pages/session-during/message-filter/Overview")}>
 		{(Component) => Component === null 
 		? <LoadingView />
 		: <Component {...props} />}
@@ -99,7 +99,7 @@ export const MessageFilterLayout = props => (
 )
 
 export const PresentationsOverview = props => (
-	<DynamicImport load={() => import("../pages/session-results/presentations/Overview")}>
+	<DynamicImport load={() => require("../pages/session-results/presentations/Overview")}>
 		{(Component) => Component === null 
 		? <LoadingView />
 		: <Component {...props} />}
@@ -107,7 +107,7 @@ export const PresentationsOverview = props => (
 )
 
 export const PresentationsDetails = props => (
-	<DynamicImport load={() => import("../pages/session-results/presentations/Details")}>
+	<DynamicImport load={() => require("../pages/session-results/presentations/Details")}>
 		{(Component) => Component === null 
 		? <LoadingView />
 		: <Component {...props} />}
@@ -115,7 +115,7 @@ export const PresentationsDetails = props => (
 )
 
 export const SurveyResultsOverview = props => (
-	<DynamicImport load={() => import("../pages/session-results/surveys/Overview")}>
+	<DynamicImport load={() => require("../pages/session-results/surveys/Overview")}>
 		{(Component) => Component === null 
 		? <LoadingView />
 		: <Component {...props} />}
@@ -125,7 +125,7 @@ export const SurveyResultsOverview = props => (
 // Superadmin
 
 export const TranslationsOverview = props => (
-	<DynamicImport load={() => import("../pages/superadmin/translations/Overview")}>
+	<DynamicImport load={() => require("../pages/superadmin/translations/Overview")}>
 		{(Component) => Component === null 
 		? <LoadingView />
 		: <Component {...props} />}
@@ -133,7 +133,7 @@ export const TranslationsOverview = props => (
 )
 
 export const TranslationsDetails = props => (
-	<DynamicImport load={() => import("../pages/superadmin/translations/Details")}>
+	<DynamicImport load={() => require("../pages/superadmin/translations/Details")}>
 		{(Component) => Component === null 
 		? <LoadingView />
 		: <Component {...props} />}
@@ -141,7 +141,7 @@ export const TranslationsDetails = props => (
 )
 
 export const EditDashboardOverview = props => (
-	<DynamicImport load={() => import("../pages/superadmin/edit-dashboard/Overview")}>
+	<DynamicImport load={() => require("../pages/superadmin/edit-dashboard/Overview")}>
 		{(Component) => Component === null 
 		? <LoadingView />
 		: <Component {...props} />}
@@ -149,7 +149,7 @@ export const EditDashboardOverview = props => (
 )
 
 export const EditDashboardDetails = props => (
-	<DynamicImport load={() => import("../pages/superadmin/edit-dashboard/Details")}>
+	<DynamicImport load={() => require("../pages/superadmin/edit-dashboard/Details")}>
 		{(Component) => Component === null 
 		? <LoadingView />
 		: <Component {...props} />}
@@ -157,7 +157,7 @@ export const EditDashboardDetails = props => (
 )
 
 export const PhonenumbersOverview = props => (
-	<DynamicImport load={() => import("../pages/superadmin/phonenumbers/Overview")}>
+	<DynamicImport load={() => require("../pages/superadmin/phonenumbers/Overview")}>
 		{(Component) => Component === null 
 		? <LoadingView />
 		: <Component {...props} />}
@@ -165,7 +165,7 @@ export const PhonenumbersOverview = props => (
 )
 
 export const PhonenumberDetails = props => (
-	<DynamicImport load={() => import("../pages/superadmin/phonenumbers/Details")}>
+	<DynamicImport load={() => require("../pages/superadmin/phonenumbers/Details")}>
 		{(Component) => Component === null 
 		? <LoadingView />
 		: <Component {...props} />}
@@ -173,7 +173,7 @@ export const PhonenumberDetails = props => (
 )
 
 export const DeleteUsersOverview = props => (
-	<DynamicImport load={() => import("../pages/superadmin/delete-users/Overview")}>
+	<DynamicImport load={() => require("../pages/superadmin/delete-users/Overview")}>
 		{(Component) => Component === null 
 		? <LoadingView />
 		: <Component {...props} />}
@@ -181,7 +181,7 @@ export const DeleteUsersOverview = props => (
 )
 
 export const SessionOverview = props => (
-	<DynamicImport load={() => import("../pages/superadmin/sessions/Overview")}>
+	<DynamicImport load={() => require("../pages/superadmin/sessions/Overview")}>
 		{(Component) => Component === null 
 		? <LoadingView />
 		: <Component {...props} />}
@@ -189,7 +189,7 @@ export const SessionOverview = props => (
 )
 
 export const SessionDetails = props => (
-	<DynamicImport load={() => import("../pages/superadmin/sessions/Details")}>
+	<DynamicImport load={() => require("../pages/superadmin/sessions/Details")}>
 		{(Component) => Component === null 
 		? <LoadingView />
 		: <Component {...props} />}
@@ -197,7 +197,7 @@ export const SessionDetails = props => (
 )
 
 export const UserOverview = props => (
-	<DynamicImport load={() => import("../pages/user/Overview")}>
+	<DynamicImport load={() => require("../pages/user/Overview")}>
 		{(Component) => Component === null 
 		? <LoadingView />
 		: <Component {...props} />}
