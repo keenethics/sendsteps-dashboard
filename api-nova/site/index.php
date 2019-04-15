4f7866d1209d698b45a9db4e2f71cfcceac9bceb
John Wheatley <john.wheatley@sendsteps.com>
2019-04-15 13:30:23 +0200

    Another test commit

--- api-nova/site/index.php
+++ api-nova/site/index.php
@@ -2,7 +2,6 @@
     require_once __DIR__."/../../api-common/errors.php";//Load Errors (just in case);
     require_once __DIR__."/../../api-common/urls.php";//Load $internalURLs Array  
     require_once __DIR__ . "/../vendor/autoload.php"; // Autoload composer packages
-
     // Load .env file for environment variables
         $dotenv = Dotenv\Dotenv::create('../');
         $dotenv->load();
