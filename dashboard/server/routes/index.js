2c76808d79b5bb029a46b9a597e437cf4e4fbdfd
NazarGorokhivskiy <nazargorokhivskiy@gmail.com>
2019-07-18 11:42:19 +0300

    started to implement getUserData method

--- dashboard/server/routes/index.js
+++ dashboard/server/routes/index.js
@@ -2,13 +2,16 @@ const express = require("express");
 const authTest = require("./authTest");
 const login = require("./login");
 const registration = require("./registration");
-const usersTest = require("../controllers/usersTest");
+const getUserData = require("../controllers/getUserData");
+//const usersTest = require("../controllers/usersTest");
 
 const router = express.Router();
 
 router.use("/auth", authTest);
 router.use("/login", login);
 router.use("/registration", registration);
+
+router.use("/getUserData", getUserData);
 // router.route("/test").get(usersTest.returnUsers);
 
 module.exports = router;
