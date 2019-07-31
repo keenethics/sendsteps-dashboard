2bf6a224eeb3fc94e0967681723fcd64e72a25d9
NazarGorokhivskiy <nazargorokhivskiy@gmail.com>
2019-07-31 16:13:16 +0300

    added delete user functionality

---
+++ dashboard/server/controllers/deleteUser.js
@@ -0,0 +1,82 @@
+const models = require('../models');
+
+const { user: User, sessions: Session } = models;
+
+function getCurrentDate() {
+  let d = new Date(),
+    month = '' + (d.getMonth() + 1),
+    day = '' + d.getDate(),
+    year = d.getFullYear();
+
+  if (month.length < 2) month = '0' + month;
+  if (day.length < 2) day = '0' + day;
+
+  return [year, month, day].join('-');
+}
+
+function makeMessageDeleted(message, appendStr) {
+  const columnLength = 32;
+  const finalMessage = message + appendStr;
+
+  if (finalMessage.length > columnLength) {
+    return message.slice(0, columnLength - appendStr.length) + appendStr;
+  }
+
+  return finalMessage;
+}
+
+async function deleteUser(req, res) {
+  const { id } = req.body;
+  const currentDate = getCurrentDate();
+  const deleteStringTemplate = '-old-' + currentDate;
+
+  try {
+    const userToDelete = await User.findOne({
+      where: {
+        id
+      }
+    });
+    const sessionToDelete = await Session.findOne({
+      where: {
+        userId: id
+      }
+    });
+
+    await User.update(
+      {
+        isDeleted: '1',
+        email: userToDelete.email + deleteStringTemplate,
+        dateDeleted: currentDate
+      },
+      {
+        where: { id }
+      }
+    );
+
+    await Session.update(
+      {
+        internetKeyword: makeMessageDeleted(sessionToDelete.internetKeyword, deleteStringTemplate),
+        textMessagingKeyword: makeMessageDeleted(
+          sessionToDelete.textMessagingKeyword,
+          deleteStringTemplate
+        )
+      },
+      {
+        where: {
+          userId: id
+        }
+      }
+    );
+
+    return res.json({
+      message: 'User was deleted!'
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send(err);
+  }
+}
+
+module.exports = {
+  deleteUser
+};
--- dashboard/src/components/common/BottomSaveBar.jsx
+++ dashboard/src/components/common/BottomSaveBar.jsx
@@ -1,41 +1,72 @@
 import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
 import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { signOut } from './../../actions/auth';
+import { post } from '../../scripts/api';
 
-class BottomSaveBar extends Component  {
+class BottomSaveBar extends Component {
+  goBack() {
+    this.props.history.goBack();
+  }
 
-    goBack() {
-        this.props.history.goBack();
-    }
-    render() {
+  deleteProfile() {
+    post(
+      '',
+      'deleteUser',
+      {},
+      result => {
+        // Signing user out
+        this.props.dispatch(signOut());
+      },
+      error => console.log(error)
+    );
+  }
 
-        const { onSave, noSave, disabled } = this.props;
+  render() {
+    const { onSave, noSave, disabled } = this.props;
 
-        return (
-            <div className="card mt-3 mb-3">
-                <div className="card-body">
-                    <div className="row">
-                        <div className="col-sm-12">
-                            {!noSave &&
-                            <div className="btn btn-success" 
-                                disabled={disabled}
-                                onClick={onSave || function(){ console.log("onSave")}}
-                            >
-                                <i className="fa fa-save"></i> Save
-                            </div>}
-                            <button 
-                                onClick={() => this.goBack()}
-                                type='button' 
-                                id='back-btn' 
-                                className='btn btn-outline-secondary float-right'>
-                                <i className="fa fa-chevron-left"></i> Back
-                            </button>
-                        </div>
-                    </div>
+    return (
+      <div className="card mt-3 mb-3">
+        <div className="card-body">
+          <div className="row">
+            <div className="col-sm-12">
+              {!noSave && (
+                <div
+                  className="btn btn-success"
+                  disabled={disabled}
+                  onClick={
+                    onSave ||
+                    function() {
+                      console.log('onSave');
+                    }
+                  }
+                >
+                  <i className="fa fa-save" /> Save
                 </div>
-            </div>            
-        )
-    }
+              )}
+              <button
+                onClick={() => this.goBack()}
+                type="button"
+                id="back-btn"
+                className="btn btn-outline-secondary float-right ml-3"
+              >
+                <i className="fa fa-chevron-left" /> Back
+              </button>
+              <button
+                onClick={() => this.deleteProfile()}
+                type="button"
+                id="delete-btn"
+                className="btn btn-danger float-right"
+              >
+                Delete account
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default withRouter(BottomSaveBar)
+export default connect()(withRouter(BottomSaveBar));
