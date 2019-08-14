1a1a5b027539750ccc10d14da3f450a9d135fac3
Maxym Fedas <maxym.fedas@gmail.com>
2019-08-14 15:50:24 +0300

    Update updateUserProfile.js

--- dashboard/server/routes/updateUserProfile.js
+++ dashboard/server/routes/updateUserProfile.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const checkJWT = require('../middlewares/checkJWT');
-const multerFileUpload = require("../middlewares/multerFileUpload");
+const { uploadFile } = require("../middlewares/multerFileUpload");
 const { updateUserProfile } = require('../controllers/updateUserProfile');
 
 const router = express.Router();
 
 router.route('/').post(
   checkJWT(),
-  multerFileUpload.uploadFile,
+  uploadFile,
   updateUserProfile
 );
 
